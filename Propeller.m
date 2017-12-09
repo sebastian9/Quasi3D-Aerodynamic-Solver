b594fc0fbeebbb4693e9fd167fb02afca0690459
Sebastian Lopez <sebastianls0228@gmail.com>
2017-12-08 22:17:58 -0600

    solve variable alpha avl

--- Propeller.m
+++ Propeller.m
@@ -3,15 +3,16 @@ avlfilename = 'a15'; % Configurar Nb a 24 y Nc a 13
 af0 = 'clarky39'; % Perfil de Núcleo
 af1 = 'clarky11'; % Perfil medio
 af2 = 'clarky08'; % Perfil punta
-Vt = 80; % m/s
+Vt = 40; % m/s
 M = 0;
 rho = 1.2250;
 dynamic_viscocity = 1.5E-05;
 
-beta75 = 35; % En grados
+beta75 = 25; % En grados
 pd15 = 0.631; % cociente paso:diámetro a 15 grados
-w = 80*pi; % rad/s
+w = 40; % rad/s
 D = 2; % m
+a = 6 * pi / 180; % Cl/°
 
-[CT, CQ] = SolverPropeller(avlfilename,af0,af1,af2,Vt,w,M,rho,dynamic_viscocity,D/2,beta75,pd15)
+[CT, CQ] = SolverPropeller(avlfilename,a,af0,af1,af2,Vt,w,M,rho,dynamic_viscocity,D/2,beta75,pd15)
 toc
--- SolverPropeller.m
+++ SolverPropeller.m
@@ -1,32 +1,35 @@
-function [CT, CQ] = SolverPropeller(avlfilename,af0,af1,af2,Vt,w,M,rho,mu,R,beta75,pd15) % Ángulos en grados
+function [CT, CQ] = SolverPropeller(avlfilename,a,af0,af1,af2,Vt,w,M,rho,mu,R,beta75,pd15) % Ángulos en grados
     
     Nb = 24;
     Sections = 8;
     dT = zeros(length(Sections),1);
     dQ = zeros(length(Sections),1);
     
-    A = beta75 - atan(Vt/(R*0.75*w))*180/pi;
-    [ wingData, S, CL, CDi ] = runAVLa(avlfilename,A);
+    [ wingData, S, CL, CDi ] = runAVLa(avlfilename,0);
 
-    for i=1:Sections
+    parfor i=1:Sections
         if i == 1
             airfoil = af0;
         else
             airfoil = interpolateAirfoils(Sections-1,i-1,af1,af2);
         end
-        Cl = wingData(1).strip{1,i*(Nb/Sections)}.cl;
         c = wingData(1).strip{1,i*(Nb/Sections)}.Chord;
-        cdi = wingData(1).strip{1,i*(Nb/Sections)}.cd;
         
-        j = 0.30 + 0.70*i/Nb;
+        j = 0.30 + 0.70*i/Sections
         
         V = sqrt(Vt^2+(w*j*R)^2)
         phi = atan(Vt/(j*R*w))*180/pi
-        beta = beta75 - 15 + atan(pd15/(pi*j))*180/pi;
+        beta = beta75 - 15 + atan(pd15/(pi*j))*180/pi
+        
+        Cl = a*(beta-phi);
         
         [ Cdprof, ai ] = stage2iterPropeller(Cl,M,V,rho,mu,c,phi,beta,airfoil);
+        
+        Cl = a*(beta-phi-ai)
+        beta-phi-ai
+        
         dL = 0.5*rho*V^2*c*Cl; % dr
-        dD = 0.5*rho*V^2*c*(cdi+Cdprof); % dr
+        dD = 0.5*rho*V^2*c*(0.0010*(Cl-0.15)^2+Cdprof); % dr
         dT(i) = dL*cos(phi+ai)-dD*sin(phi+ai);
         dQ(i) = j*R*(dL*sin(phi+ai)+dD*cos(phi+ai));
     end
